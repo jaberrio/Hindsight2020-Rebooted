f6c69c55c14040ca88e971d7535fd24d3a3fdca1
jaberrio <jaberrio88@hotmail.com>
2020-12-09 12:39:46 -0500

    Added new files

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform newPos;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(newPos.position.x,
+            newPos.position.y,
+            transform.position.z);
+    }
+}
